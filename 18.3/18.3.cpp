32f1076cc84e5472719f66c612aa4693b6caba84
bulkadany <73467445+bulkadany@users.noreply.github.com>
2021-04-12 15:25:19 +0500

    Add files via upload

---
+++ 18.3/18.3.cpp
@@ -0,0 +1,24 @@
+#include "Money.h" 
+#include <iostream> 
+using namespace std;
+
+void main()
+{
+	Money a;
+	Money b; 
+	cin>>a;
+	cin>>b;
+	--a;
+	cout << a << endl;
+	a--;
+	cout<<"a="<<a<<endl; 
+	cout<<"b="<<b<<endl;
+	if (a == b)
+	{
+		cout << "a == b" << endl;
+	}
+	else
+	{
+		cout << "a != b" << endl;
+	}
+}
